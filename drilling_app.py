5768e2add3ae12c4a5d7ff6fda851e8d4a0a892d
FreddyEcu-Ch <fpcarrio@espol.edu.ec>
2025-12-08 12:34:37 -0500

    Notebook

---
+++ drilling_app.py
@@ -0,0 +1,105 @@
+# Import Python Libraries
+
+
+
+
+
+
+
+
+# Insert an icon
+
+
+# State the design of the app
+
+
+# Inset CSS Codes to improve the design of the app
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Insert title for app
+
+
+
+
+# Add information of the app
+
+
+
+
+
+
+
+# Add additional information (expander)
+
+
+
+
+# Insert an image
+
+
+
+# Insert subheader
+
+
+# Insert a video
+
+
+
+# Insert caption
+
+
+# Sidebar section
+
+
+
+# Add title to the sidebar section
+
+
+# Upload files
+
+
+# Add sections of the app
+
+
+
+
+
+# Useful functions to activate each section
+# Function to check data
+
+
+
+
+
+
+
+# Function to visualoze 3D Wells
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Call data and sections
